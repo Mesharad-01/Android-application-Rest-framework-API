0a23832050455cf3488c1c20e7d11da5b459e0a0
Mesharad-01 <sharadmishra01official@gmail.com>
2021-08-09 22:12:42 +0530

    image folder problem and insertion constraint done

--- apitask/myapi/urls.py
+++ apitask/myapi/urls.py
@@ -3,8 +3,11 @@ from .views import myimagedetailsView, myimageView,allmyimagedetailsView
 
 
 urlpatterns = [
-    
     path('myimage/', myimageView.as_view(),name='myimageView'),
     path('myimage/<int:id>/', myimagedetailsView.as_view(),name='myimagedetailsView'),
     path('myimage/all/<int:my_id>/', allmyimagedetailsView.as_view(),name='allmyimagedetailsView'),   
 ]
+
+
+
+
